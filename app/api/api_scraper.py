07928b4c26b12a8eb03220bd076bda350cd32eca
ansi-cse <toan.do.cse@gmail.com>
2023-03-30 09:30:58 +0700

    update getImage

--- app/api/api_scraper.py
+++ app/api/api_scraper.py
@@ -1,15 +1,33 @@
 from fastapi import APIRouter
 
 from app.schemas.sche_base import DataResponse
-from app.services.scraper import scraper
+from app.services.scraper import NhaTot, Base, GetImage
 from fastapi.responses import HTMLResponse
+from fastapi.responses import ORJSONResponse
 from bs4 import BeautifulSoup
 router = APIRouter()
 
 @router.get("")
-async def getScrapercheck(url: str):
+async def getScraperFor(url: str):
     try:
-        html=scraper(url)
+        html=Base(url)
+        result=str(BeautifulSoup(html))
+        return HTMLResponse(content=result, status_code=200)
+    except: 
+        return HTMLResponse(content="Fail to load page", status_code=400)
+
+@router.get("/getImage")
+async def getScraperFor(url: str):
+    try:
+        image=GetImage(url)
+        return HTMLResponse(content=image, media_type="image/png", status_code=200)
+    except: 
+        return HTMLResponse(content="Fail to load page", status_code=400)
+
+@router.get("/nhatot")
+async def getScraperForNhaTot(url: str):
+    try:
+        html=NhaTot(url)
         result=str(BeautifulSoup(html))
         return HTMLResponse(content=result, status_code=200)
     except: 
--- app/services/scraper.py
+++ app/services/scraper.py
@@ -2,12 +2,30 @@ import undetected_chromedriver as uc
 from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.common.by import By
 from selenium.webdriver.support import expected_conditions as EC
+from fastapi.encoders import jsonable_encoder
+from fastapi.responses import JSONResponse
+import time
+from pathlib import Path
 driver = uc.Chrome(use_subprocess=True)
-def scraper(url):
+def NhaTot(url):
     with driver:
         driver.get(url)
         WebDriverWait(driver, 15).until(EC.element_to_be_clickable((By.CSS_SELECTOR, ".ShowPhoneButton_phoneButton__p5Cvt"))).click()
     return driver.page_source
+def Base(url):
+    with driver:
+        driver.get(url)
+        time.sleep(5)
+        # location=driver.execute_script("""return window.paramsMap""")
+    return driver.page_source
+def GetImage(url):
+    with driver:
+        driver.get(url)
+        time.sleep(3)
+        p = Path(url)
+        with open(p, 'wb') as file:
+            file.write(driver.save_screenshot(p))
+    return file
    
     
     
