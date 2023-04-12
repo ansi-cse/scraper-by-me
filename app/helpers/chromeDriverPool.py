3c28018af0deb410be73fa3ac4327ac1c2821299
ansi-cse <toan.do.cse@gmail.com>
2023-04-12 16:09:52 +0700

    fixed bug multiple thread

--- app/helpers/chromeDriverPool.py
+++ app/helpers/chromeDriverPool.py
@@ -8,10 +8,12 @@ class ScrapeThread(threading.Thread):
         self.page_source = None
   
     def run(self):
-        driver = uc.Chrome(version_main=112, headless=True)
-        driver.get(self.url)
-        self.page_source = driver.page_source
-        driver.close()
-    
+        try:
+            driver = uc.Chrome(version_main=112, headless=True)
+            driver.get(self.url)
+            self.page_source = driver.page_source
+            driver.close()
+        except:
+            self.page_source= "Failed to load page"    
     def get_page_source(self):
         return self.page_source
