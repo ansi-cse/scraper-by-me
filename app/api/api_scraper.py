7367cae94e9eb66553b7f11e9d343c2bae739686
ansi-cse <toan.do.cse@gmail.com>
2023-04-12 14:32:20 +0700

    advance peformance, multiple thread webdrice

--- app/api/api_scraper.py
+++ app/api/api_scraper.py
@@ -1,16 +1,25 @@
 from fastapi import APIRouter
 
-from app.services.scraper import  base, getImage, nhaTot
+from app.services.scraper import  base, getImage, nhaTot, tests, bdscom
 from fastapi.responses import HTMLResponse
 from bs4 import BeautifulSoup
 from fastapi.responses import FileResponse
 import os
-
+import cv2
+import numpy as np
+from PIL import Image, ImageEnhance
+import cv2
+import numpy as np
+import traceback
+from loguru import logger
+from app.helpers.chromeDriverPool import ScrapeThread
 router = APIRouter()
+logger.add("app.log", rotation="500 MB")
 
 @router.get("")
 async def getScraperFor(url: str):
     try:
+        logger.info("Creating new Chrome driver instance")
         html=base(url)
         result=str(BeautifulSoup(html))
         return HTMLResponse(content=result, status_code=200)
@@ -21,8 +30,17 @@ async def getScraperFor(url: str):
 async def getScraperFor(url: str):
     try:
         image=getImage(url)
-        return FileResponse(image, media_type="image/jpeg", filename="vector_image_for_you.jpg")
-    except: 
+        img = cv2.imread(image)
+        mask = cv2.imread('mask.png', cv2.IMREAD_GRAYSCALE)
+        (h,w,_)= img.shape
+        _, mask = cv2.threshold(mask, 0, 255, cv2.THRESH_BINARY)
+        # mask=cv2.resize(mask,(w, h))
+        cv2.imwrite('mask1.png', mask)
+        dst = cv2.inpaint(img, mask, 3, cv2.INPAINT_TELEA)
+        cv2.imwrite('dst.jpg', dst)
+        return FileResponse(dst, media_type="image/jpeg", filename=image)
+    except Exception:
+        traceback.print_exc()
         return HTMLResponse(content="Fail to load page", status_code=400)
 
 @router.get("/nhatot")
@@ -33,3 +51,24 @@ async def getScraperForNhaTot(url: str):
         return HTMLResponse(content=result, status_code=200)
     except: 
         return HTMLResponse(content="Fail to load page", status_code=400)
+
+@router.get("/bdscom")
+async def getScraperForNhaTot(url: str):
+    try:
+        logger.info("Creating new Chrome driver instance")
+        html=bdscom(url)
+        logger.info("Chrome driver instance created successfully")
+        result=str(BeautifulSoup(html))
+        return HTMLResponse(content=result, status_code=200)
+    except: 
+        return HTMLResponse(content="Fail to load page", status_code=400)
+
+
+@router.get("/test")
+async def test(url: str):
+    t = ScrapeThread(url)
+    t.start()
+    t.join()
+    page_source = t.get_page_source()
+    return {"results": page_source}
+    
--- app/services/scraper.py
+++ app/services/scraper.py
@@ -1,34 +1,44 @@
 import undetected_chromedriver as uc
+from selenium import webdriver
 from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.common.by import By
 from selenium.webdriver.support import expected_conditions as EC
 import time
 from pathlib import Path
 from urllib.parse import urlparse
+from loguru import logger
+from multiprocessing.pool import ThreadPool as Pool
+from app.helpers.chromeDriverPool import ScrapeThread
 
 def nhaTot(url):
-    driver = uc.Chrome(use_subprocess=False)
+    driver = uc.Chrome(use_subprocess=False,headless=True)
     with driver:
         driver.get(url)
         WebDriverWait(driver, 15).until(EC.element_to_be_clickable((By.CSS_SELECTOR, ".ShowPhoneButton_phoneButton__p5Cvt"))).click()
     result=driver.page_source
     driver.quit()
     return result
-
 def base(url):
-    driver = uc.Chrome(use_subprocess=False)
+    t = ScrapeThread(url)
+    t.start()
+    t.join()
+    page_source = t.get_page_source()
+    return page_source
+
+def bdscom(url):
+    driver = uc.Chrome(use_subprocess=False, headless=True)
     with driver:
         driver.get(url)
-        # location=driver.execute_script("""return window.paramsMap""")
+        WebDriverWait(driver, 15).until(EC.element_to_be_clickable((By.CSS_SELECTOR, ".phoneEvent.js__phone-event"))).click()
     result=driver.page_source
     driver.quit()
     return result
     
 def getImage(url):
-    driver = uc.Chrome(use_subprocess=False)
+    driver = uc.Chrome(use_subprocess=False,headless=True)
     with driver:
         driver.get(url)
-        time.sleep(0.5)
+        time.sleep(1)
         parseTofileName=urlparse(url).path.split("/")
         fileName=parseTofileName[len(parseTofileName)-1]
         p = Path(fileName)
@@ -37,6 +47,11 @@ def getImage(url):
             file.write(t)
     driver.quit()
     return fileName
+
+def tests(url):
+    for i in range(1, 100):
+        base(url)
+    return
    
     
     
